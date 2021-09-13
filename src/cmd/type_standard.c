22ff1805625472d36d2258ceb0689af186dc4c0a
Augustin LOPEZ <aulopez@e1r8p9.42.fr>
2021-09-13 11:00:09 +0200

    removed compilation warning

--- src/cmd/type_standard.c
+++ src/cmd/type_standard.c
@@ -6,11 +6,12 @@
 /*   By: aulopez <aulopez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/11 01:28:03 by aulopez           #+#    #+#             */
-/*   Updated: 2021/06/17 13:35:16 by aulopez          ###   ########.fr       */
+/*   Updated: 2021/09/13 10:55:40 by aulopez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mylib/ft_funinfo.h"
+#include "st_cmd.h"
 
 int genrsa_fun(int ac, char **av, t_funinfo *info)
 {
